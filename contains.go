c54b41c8107b769ae505e28e739c2d4e0f49ef98
Daniel Ortega <contact@daniel-ortega.mx>
2019-06-22 00:19:02 -0500

    [contains]: add documentation

--- contains.go
+++ contains.go
@@ -1,5 +1,7 @@
 package main
 
+// Contains finds an specific string into an string array
+// and then returns true or false depends on the result
 func Contains(slice []string, query string) bool {
 	for _, pointer := range slice {
 		if query == pointer {
